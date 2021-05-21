9177dd03b2b1160e8225b9e03630ee31bb2867b8
khangbdd <81902132+khangbdd@users.noreply.github.com>
2021-05-21 23:38:15 +0700

    done loginform

---
+++ main/layout/LoginForm/Components/LoginBoard.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace main.layout.LoginForm.Components
+{
+    /// <summary>
+    /// Interaction logic for LoginBoard.xaml
+    /// </summary>
+    public partial class LoginBoard : UserControl
+    {
+        public LoginBoard()
+        {
+            InitializeComponent();
+        }
+        public class TemplateConverter : IMultiValueConverter
+        {
+            public object Convert(object[] values, Type targetType, object parameter,
+                System.Globalization.CultureInfo culture)
+            {
+                string strings = "";
+                foreach (string str in values)
+                {
+                    strings += str;
+                }
+                return strings;
+            }
+
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+                System.Globalization.CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
