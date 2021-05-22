7fbabfaf952905e143fa9c346d8d4f957c748581
khangbdd <81902132+khangbdd@users.noreply.github.com>
2021-05-22 23:36:16 +0700

    done book screen

--- main/MainWindow.xaml.cs
+++ main/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Shapes;
 using main.model;
 using main.model.enums;
 using main.layout.HomeAndFeature;
+using main.layout.Book;
 
 namespace main
 {
@@ -31,6 +32,8 @@ namespace main
             this.Visibility = Visibility.Hidden;
             HomeAndFeatureTest homeAndFeatureTest = new HomeAndFeatureTest();
             homeAndFeatureTest.Show();
+            //BooksForm bookTest = new BooksForm();
+            //bookTest.Show();
 
             //Person duy = new Person("Duy@alljf.com.vn");
             //Person trang = new Person("trang");
---
+++ main/layout/Book/BooksForm.xaml.cs
@@ -0,0 +1,35 @@
+﻿using main.layout.Book.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace main.layout.Book
+{
+    /// <summary>
+    /// Interaction logic for BooksForm.xaml
+    /// </summary>
+    public partial class BooksForm : Window
+    {
+        public BooksForm()
+        {
+            InitializeComponent();
+            List<BookItem> listTest = new List<BookItem>();
+            listTest.Add(new BookItem());
+            listTest.Add(new BookItem());
+            listTest.Add(new BookItem());
+            listTest.Add(new BookItem());
+            listTest.Add(new BookItem());
+            lstBook.ItemsSource = listTest; 
+        }
+    }
+}
