00675784452ba8c39e2259de457eb737a58fc386
khangbdd <81902132+khangbdd@users.noreply.github.com>
2021-05-24 22:06:55 +0700

    second

--- main/viewmodel/features/BookListViewModel.cs
+++ main/viewmodel/features/BookListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using main.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace main.viewmodel.features
 {
-    class BookListViewModel
+    class BookListViewModel:BaseViewModel
     {
+        List<Book> lstBookItem = new List<Book>();
+
+        public List<Book> LstBookItem
+        {
+            get => lstBookItem;
+            set => lstBookItem = value;
+        }
+
+        public BookListViewModel()
+        {
+            List<Book> filter = new List<Book>();
+            filter.Add(new Book(1, "Hoàng tử bé", "novel", "13/5/2021", "Tiếng Việt", 230, 85, "Sách rất hay"));
+            filter.Add(new Book(1, "Hoàng tử ổm", "novel", "13/5/2021", "Tiếng Việt", 230, 85, "Sách rất hay"));
+            filter.Add(new Book(1, "Hoàng tử mập", "novel", "13/5/2021", "Tiếng Việt", 230, 85, "Sách rất hay"));
+            filter.Add(new Book(1, "Hoàng tử thông minh", "novel", "13/5/2021", "Tiếng Việt", 230, 85, "Sách rất hay"));
+            LstBookItem = filter;
+        }
     }
 }
