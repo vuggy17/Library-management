88045b84d94d0fb32df6fcf8b5c4fba22a01ec52
khangbdd <81902132+khangbdd@users.noreply.github.com>
2021-05-25 08:10:28 +0700

    done binding

--- main/viewmodel/features/BookListViewModel.cs
+++ main/viewmodel/features/BookListViewModel.cs
@@ -20,10 +20,15 @@ namespace main.viewmodel.features
         public BookListViewModel()
         {
             List<Book> filter = new List<Book>();
-            filter.Add(new Book(1, "Hoàng tử bé", "novel", "13/5/2021", "Tiếng Việt", 230, 85, "Sách rất hay"));
-            filter.Add(new Book(1, "Hoàng tử ổm", "novel", "13/5/2021", "Tiếng Việt", 230, 85, "Sách rất hay"));
-            filter.Add(new Book(1, "Hoàng tử mập", "novel", "13/5/2021", "Tiếng Việt", 230, 85, "Sách rất hay"));
-            filter.Add(new Book(1, "Hoàng tử thông minh", "novel", "13/5/2021", "Tiếng Việt", 230, 85, "Sách rất hay"));
+            filter.Add(new Book(1, "Hoàng tử bé","novel", "13/6/2021", "Tiếng Việt", 230, "Tân", "Sách rất hay"));
+            filter.Add(new Book(1, "Hoàng tử ốm","novel", "13/1/2001", "Tiếng Việt", 230, "Khang", "Sách rất hay"));
+            filter.Add(new Book(1, "Hoàng tử mập", "novel", "23/5/2002", "Tiếng Việt", 230, "Duy", "Sách rất hay"));
+            filter.Add(new Book(1, "Hoàng tử thông minh", "novel", "28/5/2016", "Tiếng Việt", 230, "Athur", "Sách rất hay"));
+            for (int i =0; i< filter.Count; i++)
+            {
+                filter[i].author = "Author(s): " + filter[i].author;
+                filter[i].publisher = "Publication date:" + filter[i].publisher;
+            }    
             LstBookItem = filter;
         }
     }
