ab02c7ae30e03a073a12eeeb791d0b563d974a82
Phạm Minh Tân <tan.lk16.cla@gmail.com>
2021-06-02 16:45:38 +0700

    repaire

---
+++ main/model/BookToReserve.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main.model
+{
+    class BookToReserve
+    {
+        int availableCopies;
+        int count;
+        private string _title;
+
+        public string title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        private string _author;
+
+        public string author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public int AvalableCopies
+        {
+            get => availableCopies;
+        }
+        public int Conunt
+        {
+            get => count;
+            set => count = value;
+        }
+
+        public BookToReserve(Book bookInfo)
+        {
+            this.count = 1;
+            getAvailableCopies(bookInfo);
+
+        }
+        private int getAvailableCopies(Book bookInfo)
+        {
+            int availableCopies = 0;
+            if (bookInfo != null)
+            {
+                List<BookItem> bookItems = new List<BookItem>();
+                foreach(var bookItem in bookItems)
+                {
+                    if(bookItem.info == bookInfo.id && bookItem.lendingStatus == enums.LendingStatus.AVAI)
+                    {
+                        availableCopies++;
+                    }
+                }
+            }
+            return availableCopies;
+        }
+    }
+}
--- main/viewmodel/features/ResearveBook.cs
+++ main/viewmodel/features/ResearveBook.cs
@@ -1,4 +1,5 @@
-﻿using main.model;
+﻿using main.controller;
+using main.model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +10,29 @@ namespace main.model.features
 {
     class ResearveBookViewModel: BaseViewModel
     {
-        List<Book> listBookReserve = new List<Book>();
-        public List<Book> ListBookReserve { get => listBookReserve; set => listBookReserve = value; }
+        private List<Book> listBookReserve = new List<Book>();
+        public List<Book> ListBookReserve
+        { 
+            get => listBookReserve; 
+            set => listBookReserve = value;
+        }
+        private String searchKey;
 
-
+        public String SearchKey
+        {
+            get => searchKey;
+            set
+            {
+                searchKey = value;
+                OnPropertyChanged("SearchKey");
+                OnPropertyChanged("ListBookReserve");
+            }
+        }
 
         public ResearveBookViewModel()
         {
-            List<Book> fillter = new List<Book>();
-            fillter.Add(new Book(1, "Hoàng tử bé", "novel", "13/5/2021", "Tiếng Việt", 230, "", "Sách rất hay"));
-            fillter.Add(new Book(1, "Hoàng tử ổm", "novel", "13/5/2021", "Tiếng Việt", 230, "", "Sách rất hay"));
-            fillter.Add(new Book(1, "Hoàng tử mập", "novel", "13/5/2021", "Tiếng Việt", 230, "", "Sách rất hay"));
-            fillter.Add(new Book(1, "Hoàng tử thông minh", "novel", "13/5/2021", "Tiếng Việt", 230, "", "Sách rất hay"));
-            ListBookReserve = fillter;
+            listBookReserve = DataLoadFromDB.getBooks();            
         }
+
     }
 }
